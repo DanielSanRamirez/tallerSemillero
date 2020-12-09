5235d24ef238095edae616614876489b333b9559
DanielSanRamirez <danielsanramirez@hotmail.com>
2020-12-09 11:42:37 -0500

    Servicios de base y estudiante

---
+++ src/main/java/com/clearminds/dra/servicios/ServicioBase.java
@@ -0,0 +1,27 @@
+package com.clearminds.dra.servicios;
+
+import java.sql.*;
+
+import com.clearminds.dra.bdd.ConexionBDD;
+import com.clearminds.dra.excepciones.BDDException;
+
+public class ServicioBase {
+	
+	private Connection conexion;
+	
+	public void abrirConexion() throws BDDException{
+			conexion = ConexionBDD.obtenerConexion();
+	}
+	
+	public void cerrarConexion() {
+		try {
+			if (conexion != null) {
+				conexion.close();	
+				System.out.println("Conexión cerrada");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("Error al cerrar la conexión");
+		}
+	}
+}
---
+++ src/main/java/com/clearminds/dra/servicios/ServicioEstudiante.java
@@ -0,0 +1,7 @@
+package com.clearminds.dra.servicios;
+
+public class ServicioEstudiante extends ServicioBase{
+	
+	
+
+}
